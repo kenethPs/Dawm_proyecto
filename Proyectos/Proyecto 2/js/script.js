8d1a08cc0011afe9ff32d73fae09616c79085a49
kenethPs <kenethpacheocs@gmail.com>
2022-11-30 00:42:26 -0500

    implementacion de graficos

--- Proyectos/Proyecto 2/js/script.js
+++ Proyectos/Proyecto 2/js/script.js
@@ -1,22 +1,21 @@
 window.onload = function (){
         const api = "https://ghibliapi.herokuapp.com/films";
         const HTMLResponse = document.querySelector("#contenedor");
-        
+        const listado = document.querySelector("#anios");
         fetch(api)
             .then(response => response.json())
             .then( films =>{
                 films.forEach(film => {
+                    //creacion de las cartas
                     let div1 = document.createElement("div")
                     div1.className += "col-lg-4 col-md-6 mb-4"
                     div1.className += " peliculas"
-                    
                     let div2 = document.createElement("div")
                     div2.className += "card"
                     let div3 = document.createElement("div")
                     div3.className += "bg-image hover-overlay ripple"
                     div3.setAttribute("data-mdb-ripple-color","light")
                     let image = document.createElement("img")
-
                     image.setAttribute("src",film.movie_banner)
                     image.className += "img-fluid"
                     image.className += " slideExpandUp"
@@ -36,8 +35,52 @@ window.onload = function (){
                     div4.appendChild(p)
                     div2.appendChild(div4)
                     div1.appendChild(div2)
-                    HTMLResponse.appendChild(div1)                    
-                });  
+                    HTMLResponse.appendChild(div1)
+                    //lista de opciones
+                    let anio = document.createElement("option")
+                    anio.setAttribute("value",film.release_date)
+                    anio.innerText = film.release_date
+                    listado.appendChild(anio)
+                }); 
+                
+                let arr = films
+                console.log(arr);
+                let peliculasNombre = arr.map(fil => fil.title)
+                
+                let peliculasPuntos = arr.map(fil => fil.rt_score) 
+                const data = {
+                    labels: peliculasNombre,
+                    datasets: [{
+                      label: "Popularidad",
+                      backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)'
+                      ],
+                      borderColor: [
+                        'rgb(255, 99, 132)'
+                      ],
+                      borderWidth: 1,
+                      data: peliculasPuntos,
+                    }]
+                };
+                const config = {
+                    type: 'bar',
+                    data,
+                    options: {
+                      indexAxis: 'y',
+                      scales: {
+                        y: {
+                          ticks: {
+                            crossAlign: 'far',
+                          }
+                        }
+                      }
+                    }
+                };
+                
+                let chart1 = new Chart(
+                    document.getElementById("mychart1"),
+                    config
+                );
         });
 }
 document.addEventListener("keyup", e=>{
@@ -47,12 +90,6 @@ document.addEventListener("keyup", e=>{
             pelicula.firstElementChild.querySelector("h5").textContent.toLowerCase().includes(e.target.value)
             ? pelicula.classList.remove("filtro")
             : pelicula.classList.add("filtro")
-            
-
-
         })
-
     }
-
-
 })
